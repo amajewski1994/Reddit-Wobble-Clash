fcddf5f893c238c9e80d2999c29e474505ff258a
amajewski1994 <amajewski1994@gmail.com>
2026-07-10 11:52:57 +0200

    fix camera position and controls

--- src/client/components/shared/CameraControls.tsx
+++ src/client/components/shared/CameraControls.tsx
@@ -1,5 +1,6 @@
 import { extend, ThreeElement, useThree } from '@react-three/fiber';
 import { useEffect, useRef } from 'react';
+import { MOUSE, TOUCH } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 extend({ OrbitControls });
@@ -18,5 +19,16 @@ export const CameraControls = () => {
     controlsRef.current?.update();
   }, []);
 
-  return <orbitControls ref={controlsRef} args={[camera, gl.domElement]} enableDamping />;
+  return (
+    <orbitControls
+      ref={controlsRef}
+      args={[camera, gl.domElement]}
+      enableDamping
+      enableRotate={false}
+      enableZoom={false}
+      screenSpacePanning={false}
+      mouseButtons={{ LEFT: MOUSE.PAN, MIDDLE: MOUSE.PAN, RIGHT: MOUSE.PAN }}
+      touches={{ ONE: TOUCH.PAN, TWO: TOUCH.PAN }}
+    />
+  );
 };
--- src/client/components/shared/MapCanvas.tsx
+++ src/client/components/shared/MapCanvas.tsx
@@ -4,7 +4,7 @@ import { CameraControls } from './CameraControls';
 
 export const MapCanvas = ({ children }: { children: ReactNode }) => {
   return (
-    <Canvas camera={{ position: [0, 12, 0.01], fov: 50 }}>
+    <Canvas camera={{ position: [0, 10.39, 6], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[5, 5, 5]} intensity={1} />
       {children}
